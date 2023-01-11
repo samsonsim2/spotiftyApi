f6476f8842b641d189ea7ebf5f27fe066943158f
YOUR_GITHUB_USERNAME <106437135+samsonsim2@users.noreply.github.com>
2023-01-11 23:33:31 +0800

    remove adele as default serachValue

--- src/features/searchBar/searchBarSlice.js
+++ src/features/searchBar/searchBarSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
 const initialState = {
-  searchValue: 'Adele',
+  searchValue: '',
   showAlert: false,
   alertMessage: '',
   isLoading: false,
